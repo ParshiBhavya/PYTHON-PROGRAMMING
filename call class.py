b96cb98d8846269a89a6401a0722f61940388b9a
ParshiBhavya <120474474+ParshiBhavya@users.noreply.github.com>
2022-12-27 19:27:34 +0530

    Add files via upload

---
+++ call class.py
@@ -0,0 +1,4 @@
+import classes
+object=classes.Maths()
+print(object.add(1,2))
+print(object.subt(2,1))
---
+++ classes.py
@@ -0,0 +1,18 @@
+class Maths:
+    def add(c,a,b):
+        return a+b
+    def subt(c,a,b):
+        return a-b
+    def mul(c,a,b):
+        return a*b
+
+class Chem:
+    def benzene():
+        return 'c6h6'
+    def methane():
+        return 'c4h4'
+ 
+
+    
+    
+        
