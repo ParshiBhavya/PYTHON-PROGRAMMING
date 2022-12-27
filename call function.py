7d1d9569d3ad6c76cc2b5f3ecf0171f1f9f53801
ParshiBhavya <120474474+ParshiBhavya@users.noreply.github.com>
2022-12-27 18:45:50 +0530

    Add files via upload

---
+++ call function.py
@@ -0,0 +1,16 @@
+from Mathematics import factorial
+
+print(factorial(5))
+ 
+print(factorial(15))
+
+from Mathematics import power
+print(power(5,3))
+
+from chemistry import benzene
+print(benzene())
+
+
+
+ 
+ 
---
+++ chemistry.py
@@ -0,0 +1,8 @@
+def benzene():
+    return'c6h6'
+
+def acetelyne():
+    return'c2h2'
+
+ 
+ 
